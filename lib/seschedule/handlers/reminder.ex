647127cc87dad2e67e1756123179c675e780ad2e
Leonardo de Oliveira Ramos <oliveirarleo@gmail.com>
2023-12-30 23:19:59 -0300

    fix: clean markdown for remind

--- lib/seschedule/handlers/reminder.ex
+++ lib/seschedule/handlers/reminder.ex
@@ -1,4 +1,5 @@
 defmodule Seschedule.Handlers.Remider do
+  alias Seschedule.Handlers.Search.Texts
   @doc """
   This is the handler for the remind command. It should send a message back continuing the workflow for the remind command.
   """
@@ -7,7 +8,7 @@ defmodule Seschedule.Handlers.Remider do
     {:ok, _} =
       Telegex.send_message(
         chat_id,
-        "Esta funcionalidade ainda não está disponível.",
+        Texts.clean_text_for_markdown("Esta funcionalidade ainda não está disponível ainda."),
         parse_mode: "MarkdownV2"
       )
     :ok
