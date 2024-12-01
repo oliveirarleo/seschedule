6e485ec4ede2bd2c6754b879c544bb1391a9161f
Leonardo de Oliveira Ramos <oliveirarleo@gmail.com>
2024-12-01 15:29:08 -0300

    feat: improving default variables

--- config/dev.exs
+++ config/dev.exs
@@ -1,7 +1,7 @@
 import Config
 
 config :seschedule,
-  webhook_url: "https://basilisk-right-puma.ngrok-free.app/updates_hook",
-  server_port: 8443
+  webhook_url: "https://sescheduledev.disccat.com/updates_hook",
+  server_port: 9884
 
 import_config "dev.secret.exs"
--- config/prod.exs
+++ config/prod.exs
@@ -1,7 +1,7 @@
 import Config
 
 config :seschedule,
-  webhook_url: "https://seschedule.fly.dev/updates_hook",
-  server_port: 8443
+  webhook_url: "https://seschedule.disccat.com/updates_hook",
+  server_port: 9885
 
 import_config "prod.secret.exs"
